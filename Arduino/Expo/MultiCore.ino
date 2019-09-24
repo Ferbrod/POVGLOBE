aedbe92ea34cb75ddebaea99f99ee4153cd7457f
chepecarlos <chepecarlos@alswblog.org>
2019-09-24 11:37:17 -0600

    Mejorando tiempo

--- Arduino/Expo/MultiCore.ino
+++ Arduino/Expo/MultiCore.ino
@@ -3,25 +3,25 @@ int CantidadSector = 2;
 int TiempoSector = 0;
 int Sector = 0;
 int SectorD[2] = {1, 2};
+float TiempoP = 0;
 
 void MultiCore( void * pvParameters ) {
   IniciarNeoPixel();
   while (1) {
     ActualizarTiempo();
-    //    Serial.print("TiempoVuelta:");
-    //    Serial.print(TiempoVuelta);
-    //    Serial.print(" TiempoInicial:");
-    //    Serial.print(TiempoInicial);
-    //    Serial.print(" TiempoFinal:");
-    //    Serial.println(TiempoFinal);
-    //    delay(100);
+
     if (InicioVuelta) {
       InicioVuelta = false;
       Sector = 0;
     }
-    CambiarColorAreglo(SectorD[Sector]);
-    Sector = Sector < CantidadSector ? Sector + 1 : Sector;
-    delayMicroseconds(TiempoSector);
+
+    if ( micros() > TiempoP + TiempoSector) {
+      CambiarColorAreglo(SectorD[Sector]);
+      if (Sector < CantidadSector) {
+        Sector++;
+      }
+      TiempoP = micros();
+    }
   }
 }
 
