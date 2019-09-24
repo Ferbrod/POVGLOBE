7a68fc478ec778abd8313363750da74d064c4797
chepecarlos <chepecarlos@alswblog.org>
2019-09-23 20:33:11 -0600

    Prueva de 1 segundo

--- Arduino/Expo/MultiCore.ino
+++ Arduino/Expo/MultiCore.ino
@@ -1,18 +1,24 @@
 void MultiCore( void * pvParameters ) {
   while (1) {
-    digitalWrite(Led, HIGH);
-    delay(50);
-    digitalWrite(Led, LOW);
-    delay(50);
+    //    ActualizarTiempo();
+    //    Serial.print("TiempoVuelta:");
+    //    Serial.print(TiempoVuelta);
+    //    Serial.print(" TiempoInicial:");
+    //    Serial.print(TiempoInicial);
+    //    Serial.print(" TiempoFinal:");
+    //    Serial.println(TiempoFinal);
+    //    delay(100);
+    CambiarColor(strip.Color(255,   0,   0));
+    delay(1000);
+    CambiarColor(strip.Color(0,   255,   0));
+    delay(1000);
+  }
+}
 
-    ActualizarTiempo();
-    Serial.print("TiempoVuelta:");
-    Serial.print(TiempoVuelta);
-    Serial.print(" TiempoInicial:");
-    Serial.print(TiempoInicial);
-    Serial.print(" TiempoFinal:");
-    Serial.println(TiempoFinal);
-    delay(100);
+void CambiarColor(uint32_t color) {
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+    strip.show();
   }
 }
 
